5871e24bf3ee17c7b39342657aa9498c641efd1b
Ricardo Braga <ricardobriangarcia@gmail.com>
2025-09-05 14:12:23 -0300

    Correcao StockMovement
    Inseri unit cost, required na quantidade, e os relacionamentos movi para baixo

--- Models/StockMovement.cs
+++ Models/StockMovement.cs
@@ -6,17 +6,12 @@
         //Gerado automaticamente
         public int Id { get; set; }
 
-        //Produto(Id) que está sendo movimentado
-        public int ProductId { get; set; }
-
-        public Product? Product { get; set; }
-
-        public decimal? UnitCost { get; set; }   // decimal(18,2)
-
         // Enum para facilitar a seleção do tipo In/Out
         public required MovementType Type { get; set; }
 
-        public int Quantity { get; set; } = 0;
+        public decimal? UnitCost { get; set; }   // decimal(18,2)
+
+        public required int Quantity { get; set; }
 
         //Criado no momento em que é instanciado
         public DateTime CreateDate { get; set; } = DateTime.Now;
@@ -24,10 +19,13 @@
         //Nulable para que seja opcional
         public string? Notes { get; set; }
 
+        //Produto(Id) que está sendo movimentado
+        public int ProductId { get; set; }
+        public Product? Product { get; set; }
+
         // pode ser null (para entradas ou saídas sem Centro de custo)
         public int? CostCenterId { get; set; }        
         public CostCenter? CostCenter { get; set; }
-
     }
 
     // Enum para facilitar a seleção do tipo In/Out
