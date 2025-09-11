5676910cab651a1f68fb47ff97256d2907bedc22
Ricardo Braga <ricardobriangarcia@gmail.com>
2025-09-11 09:31:53 -0300

    Products delete and search

--- Pages/Products/Index.cshtml.cs
+++ Pages/Products/Index.cshtml.cs
@@ -2,6 +2,7 @@ using EstoqueApp.Data;
 using EstoqueApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace EstoqueApp.Pages.Products
@@ -13,13 +14,64 @@ namespace EstoqueApp.Pages.Products
         => _context = context;
 
         public IList<Product> Products { get; set; } = [];
+        public SelectList Categories { get; set; } = null!;
+
+
+        [BindProperty(SupportsGet = true)]
+        public string? SearchTerm { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int? CategoryId { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public bool? IsActive { get; set; }
+
+        //[BindProperty(SupportsGet = true)]
+        //public int? MinStock { get; set; }
 
         public async Task OnGetAsync()
         {
-            Products = await _context.Products
+            // Base da busca
+            var query = _context.Products
                 .Include(p => p.Category)
-                .AsNoTracking()
-                .ToListAsync();
+                .AsQueryable();
+
+            //Filtro: TextBox
+            if (string.IsNullOrEmpty(SearchTerm) == false)
+                query = query.Where(p => p.Name.Contains(SearchTerm));
+            
+
+            // Filtro: categoria
+            if (CategoryId.HasValue)
+                query = query.Where(p => p.CategoryId == CategoryId.Value);
+
+            // Filtro: ativo/inativo
+            if (IsActive.HasValue)
+                query = query.Where(p => p.IsActive == IsActive.Value);
+
+            //// Filtro: estoque mínimo
+            //if (MinStock.HasValue)
+            //{
+            //    query = query.Where(p => p.CurrentStock >= MinStock.Value);
+            //}
+
+            Products = await query.ToListAsync();
+
+            // Preenche lista de categorias para o filtro
+            Categories = new SelectList(await _context.Categories.ToListAsync(), "Id", "Name");
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage();
         }
     }
 }
