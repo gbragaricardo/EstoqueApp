c7ca99ba3ec4e85f1a66053c011eabb49772509e
Ricardo Braga <ricardobriangarcia@gmail.com>
2025-09-11 09:29:14 -0300

    confirmModal html e js

---
+++ wwwroot/js/confirmModal.js
@@ -0,0 +1,20 @@
+﻿
+    var confirmModal = document.getElementById('confirmModal');
+    confirmModal.addEventListener('show.bs.modal', function (event) {
+      var button = event.relatedTarget;
+
+    // pega atributos do botão que abriu o modal
+    var id = button.getAttribute('data-id');
+    var handler = button.getAttribute('data-handler');
+    var message = button.getAttribute('data-message');
+
+    // seta os valores no form
+    var input = confirmModal.querySelector('#confirmModalId');
+    input.value = id;
+
+    var form = confirmModal.querySelector('#confirmModalForm');
+    form.setAttribute('action', '?handler=' + handler); // envia para o handler Razor
+
+    var msg = confirmModal.querySelector('#confirmModalMessage');
+    msg.textContent = message ?? 'Tem certeza que deseja continuar?';
+    });
