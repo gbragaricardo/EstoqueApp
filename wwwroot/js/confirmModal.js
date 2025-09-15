34199333f566dbebbdaeffde3c53135a16e85aa2
Ricardo Braga <ricardobriangarcia@gmail.com>
2025-09-15 18:38:49 -0300

    Layout e Modal de confirmacao modificados para nova UI

--- wwwroot/js/confirmModal.js
+++ wwwroot/js/confirmModal.js
@@ -1,20 +1,42 @@
-﻿
-    var confirmModal = document.getElementById('confirmModal');
-    confirmModal.addEventListener('show.bs.modal', function (event) {
-      var button = event.relatedTarget;
+﻿    // BOOTSTRAP
 
-    // pega atributos do botão que abriu o modal
-    var id = button.getAttribute('data-id');
-    var handler = button.getAttribute('data-handler');
-    var message = button.getAttribute('data-message');
+    //var confirmModal = document.getElementById('confirmModal');
+    //confirmModal.addEventListener('show.bs.modal', function (event) {
+    //  var button = event.relatedTarget;
+
+    //// pega atributos do botão que abriu o modal
+    //var id = button.getAttribute('data-id');
+    //var handler = button.getAttribute('data-handler');
+    //var message = button.getAttribute('data-message');
+
+    //// seta os valores no form
+    //var input = confirmModal.querySelector('#confirmModalId');
+    //input.value = id;
+
+    //var form = confirmModal.querySelector('#confirmModalForm');
+    //form.setAttribute('action', '?handler=' + handler); // envia para o handler Razor
+
+    //var msg = confirmModal.querySelector('#confirmModalMessage');
+    //msg.textContent = message ?? 'Tem certeza que deseja continuar?';
+    //});
+
+function openModal(button) {
+    const modal = document.getElementById('confirmModal');
+
+    // pega atributos do botão
+    const id = button.getAttribute('data-id');
+    const handler = button.getAttribute('data-handler');
+    const message = button.getAttribute('data-message');
 
     // seta os valores no form
-    var input = confirmModal.querySelector('#confirmModalId');
-    input.value = id;
+    modal.querySelector('#confirmModalId').value = id;
+    modal.querySelector('#confirmModalForm').setAttribute('action', '?handler=' + handler);
+    modal.querySelector('#confirmModalMessage').textContent = message ?? 'Tem certeza que deseja continuar?';
 
-    var form = confirmModal.querySelector('#confirmModalForm');
-    form.setAttribute('action', '?handler=' + handler); // envia para o handler Razor
+    // abre modal
+    modal.classList.remove('hidden');
+}
 
-    var msg = confirmModal.querySelector('#confirmModalMessage');
-    msg.textContent = message ?? 'Tem certeza que deseja continuar?';
-    });
+function closeModal() {
+    document.getElementById('confirmModal').classList.add('hidden');
+}
