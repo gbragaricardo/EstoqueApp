dba64aff3a211234a9115bf00b2b9e987534a36d
Ricardo Braga <ricardobriangarcia@gmail.com>
2025-09-23 17:07:12 -0300

    Criando nova exibição para transacoes

--- Pages/Movements/Index.cshtml.cs
+++ Pages/Movements/Index.cshtml.cs
@@ -1,27 +1,12 @@
-﻿using EstoqueApp.Data;
-using EstoqueApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 
 namespace EstoqueApp.Pages.Movements
 {
     public class IndexModel : PageModel
     {
-        private readonly AppDataContext _context;
-        public IndexModel(AppDataContext context)
-            =>  _context = context;
-
-        [BindProperty]
-        public IList<StockMovement> Movements { get; set; } = [];
-        public async Task OnGetAsync()
+        public void OnGet()
         {
-            Movements = await _context.StockMovements
-                .Include(m => m.Product)
-                .Include(m => m.DestinationCostCenter)
-                .AsNoTracking()
-                .ToListAsync();
         }
     }
 }
